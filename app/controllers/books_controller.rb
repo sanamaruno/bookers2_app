01028df8713b6951995056dbb402cfd913872871
sanamaruno <xwkpm269@gmail.com>
2021-08-21 10:56:16 +0000

    [Add]books/index,show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,33 @@
 class BooksController < ApplicationController
 
   def index
-  end
-
-  def show
-  end
-
-  def edit
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :books, only: [:index, :show, :edit, :create, :destroy]
+  resources :users, only: [:show]
 
 
 end
