763058cbde828611a31258611053c791fd0022a0
sanamaruno <xwkpm269@gmail.com>
2021-10-03 06:16:04 +0000

    [Add]comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,7 +2,9 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
+    # comment = current_user.book_comments.new(book_comment_params)
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
     redirect_back(fallback_location: root_path)
